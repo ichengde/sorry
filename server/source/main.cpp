d8eb278f49f93bbac34bf2f0f38a397527d37b0c
chegde <chegde@foxmail.com>
2018-07-14 00:59:20 +0800

    chores(server test): test request server, set response header field

--- server/source/main.cpp
+++ server/source/main.cpp
@@ -1,18 +1,28 @@
 #include <iostream>
 #include <cpprest/http_listener.h>
-
+#include <cpprest/http_headers.h>
 int main() {
     const auto uri = web::uri_builder{}.set_scheme("http")
             .set_host("0.0.0.0").set_port(5525).set_path("/").to_uri();
 
 
     web::http::experimental::listener::http_listener listener{uri};
-
-    listener.support(web::http::methods::GET, [](web::http::http_request message) {
+    auto handle = [](web::http::http_request message) {
         auto relativePath = web::uri::decode(message.relative_uri().path());
         auto query = web::uri::decode(message.relative_uri().query());
         auto params = web::uri::split_query(query);
         auto path = web::uri::split_path(relativePath);
+
+        auto errorHandle = [](pplx::task<void> result) {
+            try {
+                result.wait();
+                std::cout << "Sending succeeded" << std::endl;
+            }
+            catch (std::exception &e) {
+                std::cout << "Error occurred sending response: %s\n" << e.what();
+            }
+        };
+
         if (!path.empty()) {
             int i = 0;
             auto resp = web::json::value::array();
@@ -28,23 +38,26 @@ int main() {
                 i++;
             }
 
-            message.reply(web::http::status_codes::OK, resp).then([](pplx::task<void> result) {
-                try {
-                    result.wait();
-                    std::cout << "Sending succeeded" << std::endl;
-                }
-                catch (std::exception &e) {
-                    std::cout << "Error occurred sending response: %s\n" << e.what();
-                }
-            });
+            message.reply(web::http::status_codes::OK, resp).then(errorHandle);
         } else {
-            auto response = web::json::value::object();
-            response["version"] = web::json::value::string("0.0.1");
-            response["status"] = web::json::value::string("ready!");
-            message.reply(web::http::status_codes::OK, response);
+            auto content = web::json::value::object();
+            content["version"] = web::json::value::string("0.0.1");
+            content["status"] = web::json::value::string("ready!");
+
+            auto response = web::http::http_response(web::http::status_codes::OK);
+
+            response.headers().add("Access-Control-Allow-Origin","*");
+            response.headers().add("Access-Control-Allow-Headers","*");
+
+            response.set_body(content);
+
+            message.reply(response).then(errorHandle);
         }
 
-    });
+    };
+    listener.support(web::http::methods::GET, handle);
+    listener.support(web::http::methods::POST, handle);
+    listener.support(web::http::methods::OPTIONS, handle);
 
     std::cout << "sorry server start" << std::endl;
     listener.open().wait();
--- test/test.js
+++ test/test.js
@@ -1,6 +1,29 @@
-window.onerror = function(msg, file, line, col, error) {
-	    // callback is called with an Array[StackFrame]
-	StackTrace.fromError(error).then(function(stackframes){StackTrace.report(stackframes, 'http://127.0.0.1:5525/')});
+var report = function(stackframes) {
+    console.log(stackframes);
+    StackTrace.report(stackframes, 'http://127.0.0.1:5525/');
 };
 
-throw new Error('test error');
+window.onerror = function(msg, file, line, col, error) {
+    // msg, file, line, col,
+    // callback is called with an Array[StackFrame]
+    console.log(arguments);
+    if (error) {
+        StackTrace.fromError(error)
+            .then(report);
+    }
+};
+
+window.onload = function() {
+
+
+    var error = new Error('BOOM!');
+    var callback = function(stackframes) {
+        var stringifiedStack = stackframes.map(function(sf) {
+            return sf.toString();
+        }).join('\n');
+        console.log(stringifiedStack);
+    };
+
+    var errback = function(err) { console.log(err.message); };
+    StackTrace.fromError(error).then(report).catch(errback)
+}
