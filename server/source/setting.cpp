ca9a42bb5e9c6058710c24a99a24ac4767ef1882
chegde <chegde@foxmail.com>
2018-07-24 22:09:50 +0800

    chore(setting): add setting.cpp.

---
+++ server/source/setting.cpp
@@ -0,0 +1,9 @@
+#include "setting.hpp"
+
+namespace setting {
+    namespace database {
+        const std::string host;
+        const std::string user;
+        const std::string password;
+    }
+}
