038d1030991b677bb2a8fac82ab875607b1a650a
chegde <chegde@foxmail.com>
2018-07-26 23:09:40 +0800

    chore(client): update client version.

--- client/README.md
+++ client/README.md
@@ -1,6 +1,6 @@
 # Client
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 6.0.8.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 6.1.0.
 
 ## Development server
 
--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
 var report = function(stackframes) {
     console.log(stackframes);
-    StackTrace.report(stackframes, 'http://127.0.0.1:5525/');
+    StackTrace.report(stackframes, 'http://127.0.0.1:5525/stacktrace');
 };
 
 window.onerror = function(msg, file, line, col, error) {
@@ -27,3 +27,40 @@ window.onload = function() {
     var errback = function(err) { console.log(err.message); };
     StackTrace.fromError(error).then(report).catch(errback)
 }
+
+
+
+function a() {
+    console.log('a');
+    console.log(this);
+}
+a.prototype.a = 'aaa';
+
+function b() {
+    console.log('b');
+    console.log(this);
+}
+
+b.prototype.b = 'bbb';
+
+function c() {
+    console.log('c');
+    console.log(this);
+}
+c.prototype.c = 'ccc';
+
+function d() {
+    console.log('d');
+    console.log(this);
+}
+d.prototype.d = 'ddd';
+
+
+var main = function() {
+    a();
+    setTimeout(b, 0);
+    setTimeout(c, 0);
+    d();
+}
+
+main();
