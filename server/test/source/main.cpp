dcf211f62ca8f4a57eac1de7c26cdcb3b117527e
chegde <chegde@foxmail.com>
2018-08-17 01:26:03 +0800

    chore(test): add resigter test, to do link.

--- server/test/source/main.cpp
+++ server/test/source/main.cpp
@@ -1,9 +1,41 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 
 #include <iostream>
+
+#include <cpprest/http_client.h>
+#include <cpprest/json.h>
 #include "doctest.h"
 
+using namespace utility;
+using namespace web::http;
+using namespace web::http::client;
+using namespace concurrency::streams;
+
 int factorial(int number) { return number <= 1 ? number : factorial(number - 1) * number; }
+
+void resigter()
+{
+    http_client resigter_client(U("http://127.0.0.1/"));
+
+    web::json::value userData = web::json::value::object();
+    userData["user"] = web::json::value::string("js-sorry-1");
+    userData["password"] = web::json::value::string("js-sorry-password");
+
+    resigter_client.request(
+                       methods::POST,
+                       uri_builder(U("/resigter")).set_port(5525).to_string(),
+                       userData)
+        .then([=](http_response response) {
+            printf("Response status code %u returned.\n", response.status_code());
+
+            CHECK(true);
+        });
+}
+
+TEST_CASE("resigter")
+{
+    resigter();
+}
 
 TEST_CASE("testing the factorial function")
 {
